02f4d5a677056463afec46541101a10b8fc4a4dd
Jerry Liu <jerry.liu@technexion.com>
2024-01-26 15:23:38 +0800

    media: tevs: modified tevs camera supported resolution tables.

--- drivers/media/i2c/tevs/tevs_tbls.h
+++ drivers/media/i2c/tevs/tevs_tbls.h
@@ -21,59 +21,59 @@ struct resolution {
 };
 
 static struct resolution ar0144_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 60, .mode = 0},
-	{.width = 1280, .height = 720, .framerates = 60, .mode = 0},
-	{.width = 1280, .height = 800, .framerates = 60, .mode = 0},
+	{ .width = 640, .height = 480, .framerates = 60, .mode = 0 },
+	{ .width = 1280, .height = 720, .framerates = 60, .mode = 0 },
+	{ .width = 1280, .height = 800, .framerates = 60, .mode = 0 },
 };
 
 static struct resolution ar0234_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 120, .mode = 1},
-	{.width = 1280, .height = 720, .framerates = 120, .mode = 0},
-	{.width = 1920, .height = 1080, .framerates = 85, .mode = 0},
-	{.width = 1920, .height = 1200, .framerates = 60, .mode = 0},
+	{ .width = 640, .height = 480, .framerates = 120, .mode = 1 },
+	{ .width = 1280, .height = 720, .framerates = 120, .mode = 0 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 0 },
+	{ .width = 1920, .height = 1200, .framerates = 60, .mode = 0 },
 };
 
 static struct resolution ar0521_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 60, .mode = 3},
-	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
-	{.width = 1280, .height = 960, .framerates = 60, .mode = 3},
-	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
-	{.width = 2560, .height = 1440, .framerates = 30, .mode = 1},
-	{.width = 2592, .height = 1944, .framerates = 30, .mode = 1},
+	{ .width = 640, .height = 480, .framerates = 120, .mode = 3 },
+	{ .width = 1280, .height = 720, .framerates = 60, .mode = 3 },
+	{ .width = 1280, .height = 960, .framerates = 60, .mode = 3 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 1 },
+	{ .width = 2560, .height = 1440, .framerates = 32, .mode = 1 },
+	{ .width = 2592, .height = 1944, .framerates = 24, .mode = 1 },
 };
 
 static struct resolution ar0522_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 60, .mode = 3},
-	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
-	{.width = 1280, .height = 960, .framerates = 60, .mode = 3},
-	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
-	{.width = 2560, .height = 1440, .framerates = 40, .mode = 1},
-	{.width = 2592, .height = 1944, .framerates = 40, .mode = 1},
+	{ .width = 640, .height = 480, .framerates = 120, .mode = 3 },
+	{ .width = 1280, .height = 720, .framerates = 60, .mode = 3 },
+	{ .width = 1280, .height = 960, .framerates = 60, .mode = 3 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 1 },
+	{ .width = 2560, .height = 1440, .framerates = 32, .mode = 1 },
+	{ .width = 2592, .height = 1944, .framerates = 24, .mode = 1 },
 };
 
 static struct resolution ar0821_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 30, .mode = 2},
-	{.width = 1280, .height = 720, .framerates = 30, .mode = 2},
-	{.width = 1920, .height = 1080, .framerates = 30, .mode = 2},
-	{.width = 2560, .height = 1440, .framerates = 30, .mode = 0},
-	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},
+	{ .width = 640, .height = 480, .framerates = 60, .mode = 2 },
+	{ .width = 1280, .height = 720, .framerates = 60, .mode = 2 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 2 },
+	{ .width = 2560, .height = 1440, .framerates = 30, .mode = 0 },
+	{ .width = 3840, .height = 2160, .framerates = 15, .mode = 0 },
 };
 
 static struct resolution ar0822_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 30, .mode = 1},
-	{.width = 1280, .height = 720, .framerates = 30, .mode = 1},
-	{.width = 1920, .height = 1080, .framerates = 30, .mode = 1},
-	{.width = 2560, .height = 1440, .framerates = 30, .mode = 0},
-	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},
+	{ .width = 640, .height = 480, .framerates = 60, .mode = 1 },
+	{ .width = 1280, .height = 720, .framerates = 60, .mode = 1 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 1 },
+	{ .width = 2560, .height = 1440, .framerates = 30, .mode = 0 },
+	{ .width = 3840, .height = 2160, .framerates = 15, .mode = 0 },
 };
 
 static struct resolution ar1335_res_list[] = {
-	{.width = 640, .height = 480, .framerates = 60, .mode = 4},
-	{.width = 1280, .height = 720, .framerates = 60, .mode = 4},
-	{.width = 1920, .height = 1080, .framerates = 60, .mode = 3},
-	{.width = 2560, .height = 1440, .framerates = 30, .mode = 1},
-	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},
-	{.width = 4208, .height = 3120, .framerates = 15, .mode = 0},
+	{ .width = 640, .height = 480, .framerates = 60, .mode = 4 },
+	{ .width = 1280, .height = 720, .framerates = 120, .mode = 4 },
+	{ .width = 1920, .height = 1080, .framerates = 60, .mode = 3 },
+	{ .width = 2560, .height = 1440, .framerates = 30, .mode = 1 },
+	{ .width = 3840, .height = 2160, .framerates = 15, .mode = 0 },
+	{ .width = 4208, .height = 3120, .framerates = 10, .mode = 0 },
 };
 
 struct sensor_info {
@@ -121,8 +121,11 @@ static struct sensor_info tevs_sensor_table[] = {
 };
 #else
 static const int __10fps = 10;
+static const int __15fps = 15;
 static const int __20fps = 20;
+static const int __24fps = 24;
 static const int __30fps = 30;
+static const int __32fps = 32;
 static const int __60fps = 60;
 static const int __120fps = 120;
 
@@ -144,41 +147,41 @@ static const struct camera_common_frmfmt ar0234_frmfmt[] = {
 	{{1920, 1200}, &__60fps, 1, 0, 0},
 };
 static const struct camera_common_frmfmt ar0521_frmfmt[] = {
-	{{640, 480}, &__60fps, 1, 0, 3},
-	{{1280, 720}, &__30fps, 1, 0, 3},
-	{{1280, 960}, &__30fps, 1, 0, 3},
-	{{1920, 1080}, &__30fps, 1, 0, 1},
-	{{2560, 1440}, &__20fps, 1, 0, 1},
-	{{2592, 1944}, &__20fps, 1, 0, 1},
+	{{640, 480}, &__120fps, 1, 0, 3},
+	{{1280, 720}, &__60fps, 1, 0, 3},
+	{{1280, 960}, &__60fps, 1, 0, 3},
+	{{1920, 1080}, &__60fps, 1, 0, 1},
+	{{2560, 1440}, &__32fps, 1, 0, 1},
+	{{2592, 1944}, &__24fps, 1, 0, 1},
 };
 static const struct camera_common_frmfmt ar0522_frmfmt[] = {
-	{{640, 480}, &__30fps, 1, 0, 3},
-	{{1280, 720}, &__30fps, 1, 0, 3},
-	{{1280, 960}, &__30fps, 1, 0, 3},
-	{{1920, 1080}, &__30fps, 1, 0, 1},
-	{{2560, 1440}, &__20fps, 1, 0, 1},
-	{{2592, 1944}, &__20fps, 1, 0, 1},
+	{{640, 480}, &__120fps, 1, 0, 3},
+	{{1280, 720}, &__60fps, 1, 0, 3},
+	{{1280, 960}, &__60fps, 1, 0, 3},
+	{{1920, 1080}, &__60fps, 1, 0, 1},
+	{{2560, 1440}, &__32fps, 1, 0, 1},
+	{{2592, 1944}, &__24fps, 1, 0, 1},
 };
 static const struct camera_common_frmfmt ar0821_frmfmt[] = {
-	{{640, 480}, &__30fps, 1, 0, 2},
-	{{1280, 720}, &__30fps, 1, 0, 2},
-	{{1920, 1080}, &__30fps, 1, 0, 2},
-	{{2560, 1440}, &__20fps, 1, 0, 0},
-	{{3840, 2160}, &__20fps, 1, 0, 0},
+	{{640, 480}, &__60fps, 1, 0, 2},
+	{{1280, 720}, &__60fps, 1, 0, 2},
+	{{1920, 1080}, &__60fps, 1, 0, 2},
+	{{2560, 1440}, &__30fps, 1, 0, 0},
+	{{3840, 2160}, &__15fps, 1, 0, 0},
 };
 static const struct camera_common_frmfmt ar0822_frmfmt[] = {
-	{{640, 480}, &__30fps, 1, 0, 1},
-	{{1280, 720}, &__30fps, 1, 0, 1},
-	{{1920, 1080}, &__30fps, 1, 0, 1},
-	{{2560, 1440}, &__20fps, 1, 0, 0},
-	{{3840, 2160}, &__20fps, 1, 0, 0},
+	{{640, 480}, &__60fps, 1, 0, 1},
+	{{1280, 720}, &__60fps, 1, 0, 1},
+	{{1920, 1080}, &__60fps, 1, 0, 1},
+	{{2560, 1440}, &__30fps, 1, 0, 0},
+	{{3840, 2160}, &__15fps, 1, 0, 0},
 };
 static const struct camera_common_frmfmt ar1335_frmfmt[] = {
-	{{640, 480}, &__30fps, 1, 0, 4},
-	{{1280, 720}, &__30fps, 1, 0, 4},
-	{{1920, 1080}, &__30fps, 1, 0, 3},
+	{{640, 480}, &__60fps, 1, 0, 4},
+	{{1280, 720}, &__120fps, 1, 0, 4},
+	{{1920, 1080}, &__60fps, 1, 0, 3},
 	{{2560, 1440}, &__30fps, 1, 0, 1},
-	{{3840, 2160}, &__20fps, 1, 0, 0},
+	{{3840, 2160}, &__15fps, 1, 0, 0},
 	{{4208, 3120}, &__10fps, 1, 0, 0},
 };
 
