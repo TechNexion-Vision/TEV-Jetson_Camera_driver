7e93629ce73d06b0e28999a756ba01b8878729bc
Nian Yang <nian.yang@technexion.com>
2026-03-09 15:56:26 +0800

    maxim-gmsl: max96716a: improve link detection and usability and enabled pipe link remapping

--- maxim-gmsl/max96716a_mux.c
+++ maxim-gmsl/max96716a_mux.c
@@ -574,7 +574,6 @@ static int max_des_parse_link_ser_xlate(struct max_des_priv *priv)
 	ret = 0;
 	for (i = 0; i < priv->ops->num_links; i++) {
 		struct max_des_link *link = &priv->links[i];
-		struct max_des_pipe *pipe = &priv->pipes[i];
 
 		if (!link->enabled)
 			continue;
@@ -609,7 +608,6 @@ static int max_des_parse_link_ser_xlate(struct max_des_priv *priv)
 		ret = max_des_init_link_ser_xlate(priv, link, xlate->dst, xlate->src, xlate->id);
 		if (ret != 0) {
 			link->enabled = false;
-			pipe->enabled = false;
 			continue;
 		}
 	}
@@ -1809,10 +1807,10 @@ static int max96716a_check_gmsl_links(struct max_des_priv *des_priv)
 		if (current_link == -1)
 			break;
 
-		des_priv->pipes[current_link].enabled = false;
+		des_priv->links[current_link].enabled = false;
 		if ((max96716a_read(priv, link_lock_addr[current_link]) & BIT(3)) == BIT(3)) {
 			locked_links_mask |= BIT(current_link);
-			des_priv->pipes[current_link].enabled = true;
+			des_priv->links[current_link].enabled = true;
 		}
 
 		links_mask &= ~BIT(current_link);
@@ -2216,7 +2214,7 @@ static int max96716a_init_pipe(struct max_des_priv *des_priv,
 	mask = GENMASK(2 + 3 * (index - 1), 0 + 3 * (index - 1));
 	ret = max96716a_update_bits(
 							priv, 0x161, mask,
-							(pipe->stream_id + 4 * (index - 1)) << 3);
+							(pipe->stream_id + 4 * pipe->link_id) << (3 * (index - 1)));
 	if (ret)
 		return ret;
 
@@ -2307,8 +2305,10 @@ static int max96716a_init_link(struct max_des_priv *des_priv,
 	if (ret)
 		return ret;
 
-	ret = max96716a_update_bits(priv, 0x10, BIT(5), BIT(5));
-	ret += max96716a_update_bits(priv, 0x12, BIT(5), BIT(5));
+	if (index == 0)
+		ret = max96716a_update_bits(priv, 0x10, BIT(5), BIT(5));
+	else if (index == 1)
+		ret = max96716a_update_bits(priv, 0x12, BIT(5), BIT(5));
 	if (ret)
 		return ret;
 	msleep(65);
@@ -2324,8 +2324,10 @@ static int max96716a_init_link(struct max_des_priv *des_priv,
 		if (ret)
 			return ret;
 
-		ret = max96716a_update_bits(priv, 0x10, BIT(5), BIT(5));
-		ret += max96716a_update_bits(priv, 0x12, BIT(5), BIT(5));
+		if (index == 0)
+			ret = max96716a_update_bits(priv, 0x10, BIT(5), BIT(5));
+		else if (index == 1)
+			ret = max96716a_update_bits(priv, 0x12, BIT(5), BIT(5));
 		if (ret)
 			return ret;
 		msleep(65);
@@ -2463,10 +2465,10 @@ static int max96716a_post_init(struct max_des_priv *des_priv)
 	dev_dbg(des_priv->dev, "%s()\n", __func__);
 
 	for_each_subdev(des_priv, sd_priv) {
-		if (!des_priv->links[sd_priv->index].enabled)
-			continue;
-
 		pipe = &des_priv->pipes[sd_priv->pipe_id];
+
+		if (!des_priv->links[pipe->link_id].enabled)
+			continue;
 
 		fmt = max_format_by_name(pipe->code_name);
 		if (!fmt)
@@ -2492,6 +2494,7 @@ static int max96716a_post_init(struct max_des_priv *des_priv)
 }
 
 static const struct max_des_ops max96716a_ops = {
+	.supports_pipe_link_remap = true,
 	.mipi_enable = max96716a_mipi_enable,
 	.init = max96716a_init,
 	.init_phy = max96716a_init_phy,
