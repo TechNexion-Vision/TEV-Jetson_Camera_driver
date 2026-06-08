388d5d699ff63b5fd5f5ad8613e02543236a5e02
Nian Yang <nian.yang@technexion.com>
2026-06-08 18:02:09 +0800

    maxim-gmsl: max96724: replace global reset with targeted link reset
    Optimize the initialization sequence of the MAX96724 deserializer by
    replacing the full device reset with a targeted reset of the GMSL links.

--- maxim-gmsl/max96724_mux.c
+++ maxim-gmsl/max96724_mux.c
@@ -2113,7 +2113,9 @@ static int max96724_init(struct max_des_priv *des_priv)
 		if (locked_links == des_priv->gmsl_link_mask)
 			break;
 
-		max96724_reset(priv);
+		max96724_update_bits(priv, 0x18, GENMASK(7, 4), GENMASK(7, 4));
+		usleep_range(2000, 2500);
+		max96724_update_bits(priv, 0x18, GENMASK(7, 4), 0);
 		usleep_range(2000, 2500);
 	}
 
